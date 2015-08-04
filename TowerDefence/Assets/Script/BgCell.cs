c205286eb8f4a6c63cef6116f51620e581f20047
양정훈 <fog_design@nepturnz.co.kr>
2015-08-04 19:00:56 +0900

    타워 디펜스 시작

---
+++ TowerDefence/Assets/Script/BgCell.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgCell : MonoBehaviour 
+{
+    public Color white = Color.white;
+    public Color black = Color.black;
+
+    public Color startCol = Color.green;
+    public Color goalCol = Color.blue;
+
+    private tk2dSlicedSprite _spr;
+    private tk2dSlicedSprite spr;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
