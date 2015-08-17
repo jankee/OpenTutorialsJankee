0736915da21b1efbdd0119ecedbd534ddb88ffd2
양정훈 <fog_design@nepturnz.co.kr>
2015-08-17 19:25:34 +0900

    인벤 튜토리얼 시작

---
+++ INventoryTuto/Assets/Script/Inventory.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Inventory : MonoBehaviour 
+{
+    //인벤의 크기를 조정할 변수
+    private RectTransform inventoryRect;
+    //인벤의 가로와 세로 값 변수
+    private float inventoryWidth, inventoryHight;
+
+    //슬롯의 갯수 변수
+    public int slot;
+    //슬롯의 열의 갯수 변수
+    public int rows;
+
+    //슬롯의 왼쪽 간격, 윗쪽 간격 변수
+    public float slotPaddingLeft, slotPaddingTop;
+
+    //슬롯의 크기 변수
+    public float slotSize;
+
+    //슬롯 오브젝트 변수
+    public GameObject slotPrefab;
+
+    //슬롯 오브젝트 리스트 변수
+    private List<GameObject> allSlots;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
