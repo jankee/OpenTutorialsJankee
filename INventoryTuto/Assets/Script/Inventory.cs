10ad0d0a5c5c8741f199dccb7630bfd49b7fba97
양정훈 <fog_design@nepturnz.co.kr>
2015-08-18 18:29:20 +0900

    인벤 크기 제작

--- INventoryTuto/Assets/Script/Inventory.cs
+++ INventoryTuto/Assets/Script/Inventory.cs
@@ -26,12 +26,60 @@ public class Inventory : MonoBehaviour
     private List<GameObject> allSlots;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+    {
+        CreateLayout();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    private void CreateLayout()
+    {
+        //allSlot의 초기화
+        allSlots = new List<GameObject>();
+
+        int colums = slot / rows;
+
+        //스롯 싸이즈와 페딩 싸이즈로 인벤토리의 크기를 결정한다.
+        inventoryWidth = colums * (slotSize + slotPaddingLeft) + slotPaddingLeft;
+        inventoryHight = rows * (slotSize + slotPaddingTop) + slotPaddingTop;
+
+        //인벤을 RectTransform으로 초기화 한다.
+        inventoryRect = GetComponent<RectTransform>();
+        //초기화 된 인벤에 가로 세로값을 적용
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, inventoryWidth);
+        inventoryRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, inventoryHight);
+
+        for (int y = 0; y < rows; y++)
+        {
+            for (int x = 0; x < colums; x++)
+            {
+
+                //스롯의 x, y 포지션 값을 구하는 변수
+                float xPosition = slotPaddingLeft * (x + 1) + (slotSize * x);
+                float yPosition = -slotPaddingTop * (y + 1) + -(slotSize * y);
+
+                //newSlot에 slotPrefab 오브젝트를 넣어준다.
+                GameObject newSlot = (GameObject)Instantiate(slotPrefab);
+                //slotRect에 newSlot의 렉트컨포넌트를 넣준다.
+                RectTransform slotRect = newSlot.GetComponent<RectTransform>();
+                //newSlot 이름을 Slot이라 정해준다
+                newSlot.name = "Slot";
+                //newSlot의 부모를 cavas롤 변경해준다
+                newSlot.transform.SetParent(this.transform.parent);
+                //slotRect의 포지션을 정해준다
+                slotRect.localPosition = inventoryRect.localPosition + new Vector3(xPosition, yPosition, 0);
+                //사이즈 값을 slotSize로 넣어 준다
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, slotSize);
+                slotRect.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, slotSize);
+                //스케일값 변경때문에 다시 설정
+                slotRect.localScale = new Vector3(1, 1, 1);
+                //allSlot에 동록한다
+                allSlots.Add(newSlot);
+            }
+        }
+    }
 }
