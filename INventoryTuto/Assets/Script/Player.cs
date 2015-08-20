f51e8f1794bd7038b47264b2367c1f6b68b9bfb8
양정훈 <fog_design@nepturnz.co.kr>
2015-08-20 18:42:13 +0900

    아이템, 플레이어 추가

---
+++ INventoryTuto/Assets/Script/Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+    public float speed;
+
+    public Inventory inventory;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        //inventory = new Inventory();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        HandleMovement();
+	}
+
+    public void OnTriggerEnter(Collider other)
+    {
+        print("HI");
+        if (other.tag == "Item")
+        {
+            inventory.AddItem(other.GetComponent<Item>());
+        }
+    }
+
+
+
+    private void HandleMovement()
+    {
+        float translation = speed * Time.deltaTime;
+
+        this.transform.Translate(new Vector3(Input.GetAxis("Horizontal") * translation, 0, Input.GetAxis("Vertical") * translation));
+    }
+}
