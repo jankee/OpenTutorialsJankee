c07f851f52a5f349ea96401aaa88ca70e34b67f6
jankee <jjankee@gmail.com>
2015-10-19 07:59:38 +0900

    ScrollingCombat 게임 시작

---
+++ ScrollingCombatText/Assets/Scripts/CombatTextManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CombatTextManager : MonoBehaviour 
+{
+    private static CombatTextManager instance;
+    public static CombatTextManager Instance
+    {
+        get 
+        {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<CombatTextManager>();
+            }
+            return instance; 
+        }
+    }
+
+    public GameObject TextObj;
+
+    public void CreateText(Vector3 position)
+    {
+        Instantiate(TextObj, position, Quaternion.identity);
+
+        TextObj.transform.parent = GameObject.Find("Canvas").transform;
+ 
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
