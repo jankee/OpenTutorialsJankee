e956aec451c79f135767306e0d5a6bd26ccf3fc1
양정훈 <fog_design@nepturnz.co.kr>
2015-10-30 19:10:49 +0900

    text파일 로딩및 라이팅 작업 중

---
+++ GrandTour/Assets/Scripts/Book/Book.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Book : MonoBehaviour 
+{
+    public static int numberOfBooks;
+
+    //인스턴스 변수
+    private string title;
+    private string auther;
+    private int year;
+
+    //생성자 constructor
+    public Book(string _title, string _auther, int _year)
+    {
+        title = _title;
+        auther = _auther;
+        year = _year;
+    }
+
+    public string GetTitle()
+    {
+        return title;
+    }
+
+    public string GetAuther()
+    {
+        return auther;
+    }
+
+    public int GetYear()
+    {
+        return year;
+    }
+
+    public void SetTitle(string newTitle)
+    {
+        title = newTitle;
+    }
+
+    public string _ToString()
+    {
+        return title + "\n" + auther + "\n" + year;
+    }
+}
---
+++ GrandTour/Assets/Scripts/Book/MainClass.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainClass : MonoBehaviour 
+{
+    public Text testText;
+
+    public void Awake()
+    {
+        
+    }
+
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        Book b1 = new Book("Load of rings", "JRR tolken", 1950);
+
+        testText.text = b1._ToString();
+
+        print(b1._ToString());
+
+        print(b1.GetYear());
+
+        Debug.Log(b1._ToString());
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
