c16f1d046b519c637fce0ba29fa607d62a9e8a04
양정훈 <fog_design@nepturnz.co.kr>
2015-11-27 11:33:51 +0900

    수정중

--- GrandTour/Assets/Scripts/ItemScript/ItemManager.cs
+++ GrandTour/Assets/Scripts/ItemScript/ItemManager.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
 using System.Xml.Serialization;
 using System.IO;
+using System.Xml;
 
-public enum Catagory 
+public enum Catagory
 {
     EQUIPMENT,
     CONSUMEABLE,
     WEAPON,
 }
 
-public class ItemManager : MonoBehaviour {
+public class ItemManager : MonoBehaviour
+{
 
     public ItemType itemType;
 
@@ -43,44 +46,89 @@ public class ItemManager : MonoBehaviour {
 
     public int mana;
 
+    string path;
+
     public void CreateItem()
+    {
+        path = Application.streamingAssetsPath + "/Items.xml";
+
+        string result = string.Empty;
+
+        XmlItemLoadUTF8(path);
+
+        //var test = XmlItemLoadUTF8(path);
+
+        //for (int i = 0; i < test.Count; i++)
+        //{
+        //    result += i.ToString() + ": " + test[i].Weapons.ToString() + "\n";
+        //}
+
+        //ItemContainer itemContainer = new ItemContainer();
+
+        //Type[] itemTypes = { typeof(Equipment), typeof(Weapon), typeof(Consumeable) };
+
+        //FileStream fs = new FileStream(Path.Combine(Application.streamingAssetsPath, "Items.xml"), FileMode.Open);
+
+        //XmlSerializer serializer = new XmlSerializer(typeof(ItemContainer), itemTypes);
+
+        //itemContainer = (ItemContainer)serializer.Deserialize(fs);
+
+        //serializer.Serialize(fs, itemContainer);
+
+        //fs.Close();
+
+        //switch (catagoty)
+        //{
+        //    case Catagory.EQUIPMENT:
+        //        itemContainer.Eqipment.Add(new Equipment(itemName, description, itemType, quality, spriteNeutral, spriteHighlight,
+        //            maxSize, intellect, agility, stamina, strength));
+        //        break;
+        //    case Catagory.CONSUMEABLE:
+        //        itemContainer.Consumeables.Add(new Consumeable(itemName, description, itemType, quality, spriteNeutral,
+        //            spriteHighlight, maxSize, health, mana));
+        //        break;
+        //    case Catagory.WEAPON:
+        //        itemContainer.Weapons.Add(new Weapon(itemName, description, itemType, quality, spriteNeutral, spriteHighlight,
+        //            maxSize, intellect, agility, stamina, strength, attackSpeed));
+        //        print(itemContainer.Weapons.Count);
+        //        print(itemContainer.Weapons[0].ItemName);
+        //        break;
+        //}
+
+        //fs = new FileStream(Path.Combine(Application.streamingAssetsPath, "Items.xml"), FileMode.Create);
+        ////StreamWriter ffs = new StreamWriter(Application.streamingAssetsPath, fs("Windows-1252"));
+        //serializer.Serialize(fs, itemContainer);
+        //print(fs);
+        //fs.Close();
+    }
+
+    void XmlItemLoadUTF8(string path)
     {
         ItemContainer itemContainer = new ItemContainer();
 
         Type[] itemTypes = { typeof(Equipment), typeof(Weapon), typeof(Consumeable) };
-
-        FileStream fs = new FileStream(Path.Combine(Application.streamingAssetsPath, "Items.xml"), FileMode.Open);
+        
+        FileStream stream = new FileStream(path, FileMode.Open);
 
         XmlSerializer serializer = new XmlSerializer(typeof(ItemContainer), itemTypes);
 
-        itemContainer = (ItemContainer)serializer.Deserialize(fs);
+        itemContainer = (ItemContainer)serializer.Deserialize(stream);
 
-        serializer.Serialize(fs, itemContainer);
+        string strEnd = itemContainer.Consumeables.Count.ToString();
 
-        fs.Close();
+        //StreamReader streamReader = new StreamReader(stream, System.Text.Encoding.UTF8);
 
-        switch (catagoty)
-        {
-            case Catagory.EQUIPMENT:
-                itemContainer.Eqipment.Add(new Equipment(itemName, description, itemType, quality, spriteNeutral, spriteHighlight,
-                    maxSize, intellect, agility, stamina, strength));
-                break;
-            case Catagory.CONSUMEABLE:
-                itemContainer.Consumeables.Add(new Consumeable(itemName, description, itemType, quality, spriteNeutral,
-                    spriteHighlight, maxSize, health, mana));
-                break;
-            case Catagory.WEAPON:
-                itemContainer.Weapons.Add(new Weapon(itemName, description, itemType, quality, spriteNeutral, spriteHighlight,
-                    maxSize, intellect, agility, stamina, strength, attackSpeed));
-                print(itemContainer.Weapons.Count);
-                print(itemContainer.Weapons[0].ItemName);
-                break;
-        }
+        //string strEnd = streamReader.ReadToEnd();
 
-        fs = new FileStream(Path.Combine(Application.streamingAssetsPath, "Items.xml"), FileMode.Create);
-        //StreamWriter ffs = new StreamWriter(Application.streamingAssetsPath, fs("Windows-1252"));
-        serializer.Serialize(fs, itemContainer);
-        print(fs);
-        fs.Close();
+        //string strEnd01 = itemContainer.Consumeables[0].ItemName;
+
+        print(strEnd );
+
+        //string testFile = (List<ItemContainer>)serializer.Deserialize(streamReader);
+
+        //print(streamReader);
+        //return (List<ItemContainer>)serializer.Deserialize(itemContainer);
+        
+        
     }
 }
