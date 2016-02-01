b09ad69dd3d6ae86c2144a219da3030e891ff2a7
jankee <jjankee@gmail.com>
2016-02-02 08:27:07 +0900

    벌집 매니저 시작

---
+++ Beehive Management/Assets/Scripts/Queen.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Queen : MonoBehaviour 
+{
+    public Worker[] workers = new Worker[4];
+    
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        workers[0] = new Worker(new string[] { "Nectar collector", "Honey manufacturing" }); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Beehive Management/Assets/Scripts/Worker.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Worker : MonoBehaviour 
+{
+    private string curentjob = "";
+    private string[] jobsICanDo;
+
+    public Worker(string[] jobsICanDo)
+    {
+        this.jobsICanDo = jobsICanDo;
+        print("hi" + jobsICanDo);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
