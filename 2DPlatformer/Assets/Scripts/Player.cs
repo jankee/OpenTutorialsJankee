d7f39fd2eb50cc6b925d731ffbb61ac67d2aa601
양정훈 <fog_design@nepturnz.co.kr>
2016-03-04 20:01:56 +0900

    https://youtu.be/-gC1u7qHMY4?list=PLX-uZVK_0K_6VXcSajfFbXDXndb6AdBLO&t=7
    이해가 안되는 부분이 있음

---
+++ 2DPlatformer/Assets/Scripts/Player.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour 
+{
+
+    private Rigidbody2D myRigidbody;
+
+    [SerializeField]
+    private float movementSpeed;
+
+    private bool facingRight;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        facingRight = true;
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        print(horizontal);
+
+        HandleMovement(horizontal);
+
+        Flip(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+
+        myRigidbody.velocity = new Vector2(horizontal * movementSpeed, myRigidbody.velocity.y); 
+            
+        // Vector2.left;     x = -1
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && facingRight == false || horizontal < 0 && facingRight == true)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = this.transform.localScale;
+
+            theScale.x = -1;
+
+            this.transform.localScale = theScale;
+        }
+
+        //if (horizontal < 0)
+        //{
+        //    facingRight = !facingRight;
+
+        //    Vector3 theScale = this.transform.localScale;
+
+        //    theScale.x = -1;
+
+        //    this.transform.localScale = theScale;
+        //}
+        //else
+        //{
+        //    facingRight = true;
+
+        //    Vector3 theScale = this.transform.localScale;
+
+        //    theScale.x = 1;
+
+        //    this.transform.localScale = theScale;
+        //}
+    }
+}
