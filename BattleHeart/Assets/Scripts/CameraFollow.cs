9d5436e7d97e9dbe6c2a4911b3b1992bafdb61a8
user-PC\user <jjankee@gmail.com>
2018-01-12 17:41:29 +0900

    https://youtu.be/1s_rD4mvE_s?list=PLX-uZVK_0K_6JEecbu3Y-nVnANJznCzix&t=550

---
+++ BattleHeart/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform target;
+
+    private float xMin, xMax, yMin, yMax;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+    }
+
+    private void SetLimite(Vector3 minTile, Vector3 maxTile)
+    {
+        Camera cam = Camera.main;
+
+    }
+}
--- BattleHeart/Assets/Scripts/LevelManager.cs
+++ BattleHeart/Assets/Scripts/LevelManager.cs
@@ -7,19 +7,22 @@ using System;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField]
+    private Transform map;
+
+    [SerializeField]
     private Texture2D[] mapData;
 
     [SerializeField]
-    private MpaElement[] mpaElements;
+    private MapElement[] mapElements;
 
     [SerializeField]
     private Sprite defaultTile;
 
-    public Vector3 WorldStartPos
+    public Vector2 WorldStartPos
     {
         get
         {
-            return Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 0));
+            return Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
         }
     }
 
@@ -27,7 +30,7 @@ public class LevelManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        GenerateMap();
     }
 
     // Update is called once per frame
@@ -38,13 +41,37 @@ public class LevelManager : MonoBehaviour
 
     public void GenerateMap()
     {
+        int width = mapData[0].width;
+        int height = mapData[0].height;
+
         for (int i = 0; i < mapData.Length; i++)
         {
             for (int x = 0; x < mapData[i].width; x++)
             {
                 for (int y = 0; y < mapData[i].height; y++)
                 {
+                    Color colorData = mapData[i].GetPixel(x, y);
 
+                    print(colorData);
+
+                    MapElement newElement = Array.Find(mapElements, e => e.MyColor == colorData);
+
+                    if (newElement != null)
+                    {
+                        float xPos = WorldStartPos.x + (defaultTile.bounds.size.x * x);
+                        float yPos = WorldStartPos.y + (defaultTile.bounds.size.y * y);
+
+                        GameObject go = Instantiate(newElement.MyElementPrefab);
+
+                        go.transform.position = new Vector2(xPos, yPos);
+
+                        if (newElement.MyElementPrefab.tag == "Obstacle")
+                        {
+                            go.GetComponent<SpriteRenderer>().sortingOrder = width * 2 - height * 2;
+                        }
+
+                        go.transform.SetParent(map);
+                    }
                 }
             }
         }
@@ -53,7 +80,7 @@ public class LevelManager : MonoBehaviour
 }
 
 [Serializable]
-public class MpaElement
+public class MapElement
 {
     [SerializeField]
     private string tileTag;
