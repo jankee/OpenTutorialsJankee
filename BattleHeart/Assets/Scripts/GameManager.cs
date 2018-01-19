5d403b64a78255c46c207d4917985b299093e916
user-PC\user <jjankee@gmail.com>
2018-01-19 17:54:46 +0900

    no message

---
+++ BattleHeart/Assets/Scripts/GameManager.cs
@@ -0,0 +1,165 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    //선택된 플레이어를 담기 위한 변수
+    private Player player;
+
+    //마우스 움직임 확인
+    private bool bMove = false;
+
+    //타겟 선택 시 생성되는 오브젝트
+    [SerializeField]
+    private GameObject mTarget;
+
+    private GameObject tmp;
+
+    private Coroutine moveRoutine;
+
+    // Use this for initialization
+    private void Start()
+    {
+        player = null;
+        moveRoutine = null;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        OnRayCaset();
+    }
+
+    //인풋 메소드
+    public void HandleInput(Ray ray, RaycastHit hitInfo)
+    {
+        //마우스 눌러졌을 때
+        if (Input.GetMouseButtonDown(0))
+        {
+            ///선택한 플레이어가 이미 선택 된 플레이어인지 확인
+            if (player != null && hitInfo.transform.name == player.name)
+            {
+                print("선택 되어있는것이랑 같다");
+                tmp = Instantiate(mTarget, hitInfo.transform.position, Quaternion.identity);
+            }
+            else if (player != null && hitInfo.transform.name != player.name)
+            {
+                print("선택 되어있는거랑 다르다");
+                player = hitInfo.transform.GetComponent<Player>();
+
+                tmp = Instantiate(mTarget, hitInfo.transform.position, Quaternion.identity);
+            }
+            else
+            {
+                print("선택 안되어 있다");
+                //플레이어를 선택했을때
+                if (hitInfo.transform.tag == "Player")
+                {
+                    player = hitInfo.transform.GetComponent<Player>();
+
+                    if (!player.MyMove)
+                    {
+                        tmp = Instantiate(mTarget, hitInfo.transform.position, Quaternion.identity);
+                    }
+                }
+                else if (hitInfo.transform.tag == "Enemy")
+                {
+                    print("Enemy");
+                }
+                else
+                {
+                    if (moveRoutine != null)
+                    {
+                        StopCoroutine(moveRoutine);
+                    }
+
+                    if (player != null)
+                    {
+                        player = null;
+                    }
+                    print("None");
+                }
+            }
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            ///드래그 중에는 타겟이 마우스를 따라 다닌다.
+            Vector3 vMovePos = new Vector3(hitInfo.point.x, 0, hitInfo.point.z);
+
+            tmp.transform.position = vMovePos;
+
+            //print(hitInfo.point);
+
+            Debug.DrawRay(ray.origin, hitInfo.point, Color.red);
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            ///선택한 플레이어가 이미 선택 된 플레이어인지 확인
+            if (player != null && hitInfo.transform.name == player.name)
+            {
+                if (moveRoutine != null)
+                {
+                    StopCoroutine(moveRoutine);
+                }
+
+                moveRoutine = StartCoroutine(player.Move(hitInfo.transform.position));
+
+                Destroy(tmp.gameObject);
+
+                print("선택 되어있는것이랑 같다");
+            }
+            else if (player != null && hitInfo.transform.name != player.name)
+            {
+                print("선택 되어있는거랑 다르다");
+
+                moveRoutine = StartCoroutine(player.Move(hitInfo.transform.position));
+
+                Destroy(tmp.gameObject);
+            }
+            else
+            {
+                ///플레이어가 같다면 새로운 포인트로 이동
+                ///에너미를 선택을 하면 공격
+                //tmp.transform.position = hitInfo.transform.position;
+
+                //if (moveRoutine != null)
+                //{
+                //    StopCoroutine(moveRoutine);
+                //}
+
+                //if (player != null)
+                //{
+                //    moveRoutine = StartCoroutine(player.Move(hitInfo.transform.position));
+                //}
+
+                //Destroy(tmp.gameObject);
+
+                //bMove = false;
+            }
+
+            //if (!player.MyMove)
+            //{
+        }
+    }
+
+    public void OnRayCaset()
+    {
+        //마우스 위치에서 레이를 쏜다
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(ray, out hitInfo, 255))
+        {
+            HandleInput(ray, hitInfo);
+        }
+
+        print("시작");
+    }
+
+    public void OnDragEnd()
+    {
+        print("끝");
+    }
+}
---
+++ BattleHeart/Assets/Scripts/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    //캐릭터 무브 스피드
+    [SerializeField]
+    private float mSpeed;
+
+    private bool bMove = false;
+
+    public bool MyMove
+    {
+        get
+        {
+            return bMove;
+        }
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        bMove = false;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    public IEnumerator Move(Vector3 moveEnd)
+    {
+        print("움직임");
+
+        float distance = Vector3.Distance(this.transform.position, moveEnd);
+
+        while (distance >= 0.5f)
+        {
+            //bMove = true;
+
+            this.transform.position = Vector3.MoveTowards(this.transform.position, moveEnd, mSpeed * Time.deltaTime);
+
+            yield return null;
+
+            distance = Vector3.Distance(this.transform.position, moveEnd);
+        }
+
+        //bMove = false;
+
+        print("움직임 끝");
+    }
+}
